8c04f9e773cda76b270c3033ba100ae514affa4c
Dyadyusha Artem <artikool5@yandex.ru>
2024-05-21 11:35:07 +0300

    nvim: update to 0.10

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.root_dir = opts.root_dir
-        or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
-      opts.sources = vim.list_extend(opts.sources or {}, {})
-      nls.builtins.formatting.biome.with({
-        filetype = { "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescriptreact" },
-        args = {
-          "check",
-          "--apply-unsafe",
-          "--formatter-enabled=true",
-          "--organize-imports-enabled=true",
-          "--skip-errors",
-          "--stdin-file-path=$FILENAME",
-        },
-      })
-    end,
-  },
+  -- {
+  --   "nvimtools/none-ls.nvim",
+  --   opts = function(_, opts)
+  --     local nls = require("null-ls")
+  --     opts.root_dir = opts.root_dir
+  --       or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
+  --     opts.sources = vim.list_extend(opts.sources or {}, {})
+  --     nls.builtins.formatting.biome.with({
+  --       filetype = { "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescriptreact" },
+  --       args = {
+  --         "check",
+  --         "--apply-unsafe",
+  --         "--formatter-enabled=true",
+  --         "--organize-imports-enabled=true",
+  --         "--skip-errors",
+  --         "--stdin-file-path=$FILENAME",
+  --       },
+  --     })
+  --   end,
+  -- },
   -- {
   --   "neovim/nvim-lspconfig",
   --   ---@class PlaginLspOpts
