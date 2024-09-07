e3d64e3f974ccb1f59945683f1e193c7fb60a479
Dyadyusha Artem <artikool5@yandex.ru>
2024-09-07 18:22:23 +0300

    nvim: add styled-components support

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,24 @@
 return {
 	{ "olrtg/nvim-emmet" },
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {
+			settings = {
+				separate_diagnostic_server = true,
+				tsserver_plugins = { "@styled/typescript-styled-plugin" },
+				publish_diagnostic_on = "insert_leave",
+				tsserver_path = nil,
+				tsserver_max_memory = "auto",
+				complete_function_calls = false,
+				include_completions_with_insert_text = true,
+				code_lens = "off",
+				disable_member_code_lens = true,
+				jsx_close_tag = {
+					enable = false,
+					filetypes = { "javascriptreact", "typescriptreact" },
+				},
+			},
+		},
+	},
 }
