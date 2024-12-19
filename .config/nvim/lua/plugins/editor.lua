e664b9ade44f2128ebded97ffdaa080d716994a4
Dyadyusha Artem <artikool5@yandex.ru>
2024-12-19 13:33:12 +0300

    nvim: add multicursor

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,6 +1,23 @@
 return {
-	"folke/flash.nvim",
-	opts = {
+	{ "folke/flash.nvim", opts = {
 		labels = "aoeuhtnsidpgcrlqjkvwmbxyfz",
+	} },
+	{
+		"smoka7/multicursors.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"nvimtools/hydra.nvim",
+		},
+		opts = {},
+		cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+		keys = {
+			{
+				mode = { "v", "n" },
+				"<Leader>m",
+				"<cmd>MCstart<cr>",
+				desc = "Create a selection for selected text or word under the cursor",
+			},
+		},
 	},
+	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
