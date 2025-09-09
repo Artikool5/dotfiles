bbbba64fe6147a06c3fe2a786cc66fe290a6d8e9
Dyadyusha Artem <artikool5@yandex.ru>
2025-09-09 16:17:01 +0300

    nvim: add liveserver support

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -3,6 +3,11 @@ return {
 		labels = "aoeuhtnsidpgcrlqjkvwmbxyfz",
 	} },
 	{
+		"barrett-ruth/live-server.nvim",
+		build = "pnpm add -g live-server",
+		cmd = { "LiveServerStart", "LiveServerStop" },
+		config = true,
+	},
 		},
 			},
 		},
