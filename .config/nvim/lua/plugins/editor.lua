cc9163d69d7a5c876019077147d4c3597dd853fa
Dyadyusha Artem <artikool5@yandex.ru>
2025-09-09 16:19:06 +0300

    nvim: disable autocomplete while typing

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -8,7 +8,20 @@ return {
 		cmd = { "LiveServerStart", "LiveServerStop" },
 		config = true,
 	},
+	{
+		"saghen/blink.cmp",
+		-- Don't show completion menu until key is pressed
+		opts = {
+			completion = {
+				-- Completion still works in the background and it's possible to `C-y` to insert
+				-- Uncomment to disable completely
+				trigger = { show_on_keyword = false, show_on_trigger_character = false },
+				menu = {
+					auto_show = false,
+				},
+			},
 		},
+	},
 			},
 		},
 	},
