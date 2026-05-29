c37532e03bf9fb3e45afafbbcde2d30cb9604f4b
Dyadyusha Artem <artikool5@yandex.ru>
2026-05-29 14:01:19 +0300

    nvim: add keymap to append ";" at line end

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set('n', '<leader>;', function()
+    local line = vim.fn.getline('.')
+    vim.fn.setline(".", line .. ";")
+end, { desc = "Append semicolon at end of line" })
