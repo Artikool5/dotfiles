7f8e8da179f567565985a0a66a77936ccab6fe05
Dyadyusha Artem <artikool5@yandex.ru>
2026-06-03 13:41:38 +0300

    nvim: don't preselect first option in autocompletion

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -8,20 +8,26 @@ return {
     cmd = { "LiveServerStart", "LiveServerStop" },
     config = true,
   },
-  -- {
-  -- 	"saghen/blink.cmp",
-  -- 	-- Don't show completion menu until key is pressed
-  -- 	opts = {
-  -- 		completion = {
-  -- 			-- Completion still works in the background and it's possible to `C-y` to insert
-  -- 			-- Uncomment to disable completely
-  -- 			trigger = { show_on_keyword = false, show_on_trigger_character = false },
-  -- 			menu = {
-  -- 				auto_show = false,
-  -- 			},
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "saghen/blink.cmp",
+    -- Stick to stable version to get binaries for faster fuzzy matching
+    version = "*",
+    opts = {
+      completion = {
+        -- trigger = { show_on_keyword = false, show_on_trigger_character = false },
+        -- menu = {
+        --   auto_show = false,
+        --   auto_show_delay_ms = 0,
+        -- },
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = true,
+          },
+        },
+      },
+    },
+  },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
